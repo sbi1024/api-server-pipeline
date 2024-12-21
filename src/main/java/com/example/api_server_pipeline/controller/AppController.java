5f99e8404b3b8e3d7f3e72a1f690e1a143bca0b8
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2024-12-21 11:42:05 +0900

    deploy.yml 파일 생성

--- src/main/java/com/example/api_server_pipeline/controller/AppController.java
+++ src/main/java/com/example/api_server_pipeline/controller/AppController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
     @GetMapping("/")
     public String index() {
-        return "Hello World";
+        return "Hello World, GitHub Actions CI/CD Success!";
     }
 }
